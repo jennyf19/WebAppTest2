a2e35f960073b94501ba09005a388bb119e599b9
jeferrie <jeferrie@MININT-2J4E4HS>
2016-11-10 11:59:44 -0800

    Add project files.

---
+++ WebAppTest2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppTest2.Startup))]
+namespace WebAppTest2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
